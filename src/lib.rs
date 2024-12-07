5192cc34c9b229a914f24d631127047d460b93a2
Victor <gafiatulin@gmail.com>
2024-12-07 10:50:06 +0100

    feat: day 7

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ extern crate aoc_runner;
 
 #[macro_use]
 extern crate aoc_runner_derive;
+extern crate core;
 
 pub mod day1;
 pub mod day2;
@@ -9,5 +10,6 @@ pub mod day3;
 pub mod day4;
 pub mod day5;
 pub mod day6;
+pub mod day7;
 
 aoc_lib! { year = 2024 }
